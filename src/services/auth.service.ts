dee0956f3cbb7262f927633dc8c9bdcb8b524e3b
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-01-04 14:42:23 -0400

    Fazendo o App Aproveitar o Usuário Logado na Tela Inicial

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -15,6 +15,13 @@ export class AuthService {
 
     }
 
+    refreshToken(){
+        return this.http.post(`${API_CONFIG.baseUrl}/auth/refresh_token`,{}, {
+            observe: 'response', 
+            responseType: 'text'
+        });
+    }
+
     authenticate(creds : CredenciaisDTO){
         return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
             observe: 'response', 
