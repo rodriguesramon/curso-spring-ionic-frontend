d7b2b3bfeb55b943815605ff9c5c5c5565712691
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-01-05 08:38:21 -0400

    Salvando Novo Cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -29,11 +30,18 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj);
 
             switch(errorObj.status){
-                case 401 : this.handle401()
+                case 401 : 
+                    this.handle401()
                 break;
+                
+                case 422 : 
+                    this.handle422(errorObj)
+                break;
+
                 case 403 :
                     this.handle403();
-                    break;
+                break;
+
                 default : this.handleDefaultError(errorObj);
             }
 
@@ -71,6 +79,27 @@ export class ErrorInterceptor implements HttpInterceptor {
         this.storage.setLocalUser(null);
     }
 
+    handle422(errorObj){
+        let alert = this.alertCtrl.create({
+            title : "Erro 422: Validação",
+            message : this.listErrors(errorObj.errors),
+            enableBackdropDismiss : false,
+            buttons : [
+                {
+                    text : "Ok"
+                }
+            ]
+        })
+        alert.present();
+    }
+    
+    private listErrors(messages : FieldMessage[]) : string {
+        let s : string = '';
+        for (var i=0; i<messages.length; i++) {
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+        return s;
+    }
 }
 
 export const ErrorInterceptorProvider = {
