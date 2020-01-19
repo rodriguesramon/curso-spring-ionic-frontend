ee94be6306114ced7647301c20ee7a10d3b6ec4b
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-01-19 16:01:19 -0400

    Infinite Scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,8 @@ import { ProdutoService } from '../../services/domain/produto.service';
 })
 export class ProdutosPage {
 
-  items : ProdutoDTO[];
+  items : ProdutoDTO[] = [];
+  page : number = 0;
 
   constructor(
     public navCtrl: NavController, 
@@ -25,8 +26,8 @@ export class ProdutosPage {
     
   };
 
-  loadImageUrls() {
-    for (var i=0; i<this.items.length; i++) {
+  loadImageUrls(start : number, end : number) {
+    for (var i = start; i <= end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
         .subscribe(response => {
@@ -51,11 +52,13 @@ export class ProdutosPage {
   loadData(){
     let categoria_id = this.navParams.get("categoria_id");
     let loader = this.presentLoading();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
     .subscribe(response => {
-      this.items = response["content"];
+      let start = this.items.length;
+      this.items = this.items.concat(response["content"]);
+      let end = this.items.length - 1;
       loader.dismiss();
-      this.loadImageUrls();
+      this.loadImageUrls(start, end);
     }, 
     error => {
       loader.dismiss();
@@ -63,10 +66,20 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0;
+    this.items = [];
     this.loadData();
     setTimeout(() => {   
       refresher.complete();
     }, 1000);
+  }
+
+  doInfinite(infiniteScroll){
+    this.page++;
+    this.loadData();
+    setTimeout(() => {
+      infiniteScroll.complete();
+    }, 100);
   }
 
 }
