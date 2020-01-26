0aeb51ea838b30985072a781816b390b1abb9a77
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-01-26 00:08:08 -0400

    Fazendo o Upload da Foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -34,6 +34,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData(){
     let localUser = this.storage.getLocalUser();
     if(localUser && localUser.email){
         this.clienteService.findByEmail(localUser.email)
@@ -76,6 +80,21 @@ export class ProfilePage {
     }, (err) => {
      // Handle error
     });
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      }, 
+      error => {
+
+      });
+  }
+
+  cancel(){
+    this.picture = null;
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -4,11 +4,15 @@ import { Observable } from "rxjs";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
+import { ImageUtilService } from "../image-util.service";
 
 @Injectable()
 export class ClienteService {
 
-    constructor(public http : HttpClient, public storage : StorageService){
+    constructor(
+        public http : HttpClient, 
+        public storage : StorageService,
+        public imageUtilService : ImageUtilService){
 
     }
 
@@ -22,6 +26,7 @@ export class ClienteService {
 
     getImageFromBucket(id : string) : Observable<any> {
         let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
+        
         return this.http.get(url, {responseType : 'blob'});
     }
 
@@ -29,6 +34,20 @@ export class ClienteService {
         return this.http.post(
             `${API_CONFIG.baseUrl}/clientes`, 
             obj,
+            { 
+                observe: 'response', 
+                responseType: 'text'
+            }
+        ); 
+    }
+
+    uploadPicture(picture){
+        let pictureBlob = this.imageUtilService.dataUriToBlob(picture);
+        let formData : FormData = new FormData();
+        formData.set("file",pictureBlob, "file.png");
+        return this.http.post(
+            `${API_CONFIG.baseUrl}/clientes/picture`, 
+            formData,
             { 
                 observe: 'response', 
                 responseType: 'text'
