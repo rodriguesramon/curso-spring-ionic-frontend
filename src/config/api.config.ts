4ee17e74d1bca67ce270713e0a7a2a35c697e83b
Ramon Rodrigues <ramon.rodrigues.lima@gmail.com>
2020-02-09 12:14:34 -0400

    Testando o App no Dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://final-app-spring.herokuapp.com",
     bucketBaseUrl : "https://curso-spring-ionic-ramon.s3.sa-east-1.amazonaws.com"
 }
